535e6292782029099c39904b10af05eb6f7fb397
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-08-22 19:55:42 +0300

    Added loading of data from text file

--- build.py
+++ build.py
@@ -1,3 +1,13 @@
+import os.path
+
+
+def load_text_data(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file:
+        return file.read()
+
+
 def main():
     pass
 
