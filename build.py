82120c077195c0fd39034284c7ba114624ddb723
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-08-23 15:07:08 +0300

    Added getting of template from templates directory

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os.path
 import json
 
+from jinja2 import Environment, FileSystemLoader
+
 
 def load_text_data(filepath):
     if not os.path.exists(filepath):
@@ -14,6 +16,13 @@ def load_json_data(filepath):
         return None
     with open(filepath, 'r') as file:
         return json.load(file)
+
+
+def get_template(templates_dir, template_filename):
+    file_loader = FileSystemLoader(templates_dir)
+    env = Environment(loader=file_loader)
+
+    return env.get_template(template_filename)
 
 
 def main():
