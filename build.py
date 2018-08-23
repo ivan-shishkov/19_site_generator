4f9846405130f477a1b65fcb298b1ffa4d2d7d5d
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-08-23 19:29:27 +0300

    Added generating of site's articles as html pages from Markdown files

--- build.py
+++ build.py
@@ -44,8 +44,66 @@ def add_destination_filepath(articles_info):
         )
 
 
+def make_site_articles(articles_info, articles_dir,
+                       article_template, markdown_converter, output_dir):
+    for article_info in articles_info:
+        article_html = markdown_converter.reset().convert(
+            source=load_text_data(
+                filepath=os.path.join(articles_dir, article_info['source']),
+            ),
+        )
+        article_html_dir = os.path.join(
+            output_dir,
+            os.path.dirname(article_info['destination']),
+        )
+
+        if not os.path.exists(article_html_dir):
+            os.mkdir(article_html_dir)
+
+        article_template.stream(
+            content=article_html,
+            title=article_info['title'],
+        ).dump(fp=os.path.join(
+            output_dir,
+            article_info['destination'],
+        ))
+
+
+def make_site(site_config_info, articles_dir, templates_dir,
+              article_template_filename, index_page_template_filename,
+              output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    markdown_converter = get_markdown_converter()
+
+    make_site_articles(
+        articles_info=site_config_info['articles'],
+        articles_dir=articles_dir,
+        article_template=get_template(
+            templates_dir,
+            article_template_filename,
+        ),
+        markdown_converter=markdown_converter,
+        output_dir=output_dir,
+    )
+
+
 def main():
-    pass
+    site_config_info = load_json_data('config.json')
+
+    add_destination_filepath(
+        articles_info=site_config_info['articles']
+    )
+
+    make_site(
+        site_config_info=site_config_info,
+        articles_dir='articles',
+        templates_dir='templates',
+        article_template_filename='article.html',
+        index_page_template_filename='index.html',
+        output_dir='rendered_site',
+    )
 
 
 if __name__ == '__main__':
